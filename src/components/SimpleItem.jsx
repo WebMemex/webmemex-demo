e02d014eb76aca5fb2bca2dde1e34862f6409a8b
Gerben <gerben@treora.com>
2016-06-22 16:38:07 +0200

    Use contentEditable div to edit notes

--- src/components/SimpleItem.jsx
+++ src/components/SimpleItem.jsx
@@ -1,15 +1,53 @@
 import React from 'react'
+import { bindActionCreators } from 'redux'
+import { connect } from 'react-redux'
+import ContentEditable from 'react-contenteditable'
+
+import storage from '../storage'
 
 let SimpleItem = React.createClass({
 
     render() {
+        // Only enable editing of the centered item (for now)
+        this.editingEnabled = this.props.canvasItem.centered
+        let html = this.props.text
         return (
-            <div className='simple-item'>
-                <p className='simple-item-text'>{this.props.text}</p>
-            </div>
+            <ContentEditable
+                ref='content'
+                html={html}
+                disabled={!this.editingEnabled}
+                onChange={event => this.props.handleChange(event.target.value)}
+                className='simple-item'
+                userIsKing
+            />
         )
-    }
+    },
+
+    // When the item is centered, let the editable div grab keyboard input
+    componentDidMount() {
+        if (this.props.canvasItem.centered) {
+            this.refs['content'].htmlEl.focus()
+        }
+    },
+    componentDidUpdate(oldProps) {
+        if (!oldProps.canvasItem.centered
+           && this.props.canvasItem.centered) {
+            this.refs['content'].htmlEl.focus()
+        }
+    },
 
 })
 
-export default SimpleItem
+
+function mapStateToProps(state) {
+    return {
+    }
+}
+
+function mapDispatchToProps(dispatch, {docId}) {
+    return bindActionCreators({
+        handleChange: text => storage.updateNoteText({docId, text})
+    }, dispatch)
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(SimpleItem)
