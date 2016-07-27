d8f6b30dbb00fc653321d69dab15f51af82c8575
Gerben <gerben@treora.com>
2016-07-27 15:47:22 +0300

    Process intercepted link clicks

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import { Provider } from 'react-redux'
 import makeStore from './store'
 import { Canvas } from './canvas'
 import StemItem from './components/StemItem'
-import { initCanvas } from './actions'
+import * as actions from './actions'
 
 const store = makeStore()
 
@@ -18,7 +18,16 @@ ReactDOM.render(
 
 // Place our initial content
 // (delay to wait for redux-pouchdb to read storage state) // TODO neaten
-window.setTimeout(()=>store.dispatch(initCanvas()), 200)
+window.setTimeout(()=>store.dispatch(actions.initCanvas()), 200)
+
+// Listen for link clicks
+window.addEventListener('message', function (m) {
+    let url = m.data.url
+    console.log('Received URL: ' + url)
+    if (url !== undefined) {
+        store.dispatch(actions.navigateFromLink({url}))
+    }
+})
 
 // TESTING
 window.store=store
