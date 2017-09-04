5a5ad726e7c3ae45336eb98e0a8138f74977eab3
Gerben <gerben@treora.com>
2017-09-04 16:42:06 +0200

    Only show the url for tiny webpages.
    For performance.

--- src/components/Webpage.jsx
+++ src/components/Webpage.jsx
@@ -1,16 +1,17 @@
 import React from 'react'
 
 let Webpage = React.createClass({
+    getInitialState() {
+        return {hasIFrame: false}
+    },
 
-    render() {
-        let canvasItemHeight = this.props.height
-        let canvasItemWidth = this.props.width
+    calculateStyle(props) {
+        let canvasItemHeight = props.height
+        let canvasItemWidth = props.width
         let minFramePageHeight = Math.max(canvasItemHeight, 600)
         let minFramePageWidth = Math.max(canvasItemWidth, 800)
         let scale = Math.min(canvasItemHeight / minFramePageHeight,
                              canvasItemWidth / minFramePageWidth)
-
-        let expanded = this.props.canvasItem.expanded
 
         let wrapperStyle = {
             height: canvasItemHeight,
@@ -22,8 +23,37 @@ let Webpage = React.createClass({
             transform: 'scale('+scale+')',
         }
 
-        let iframeSrcPrefix = process.env.MEMEX_PROXY_URL_PREFIX || ''
+        const showIFrame = props.canvasItem.centered || scale > 0.1
 
+        return {scalingStyle, wrapperStyle, showIFrame}
+    },
+
+    componentWillReceiveProps(nextProps) {
+        let {showIFrame} = this.calculateStyle(nextProps)
+        // If the iframe has been shown once, remember to keep it there.
+        if (showIFrame) {
+            this.setState({hasIFrame: true})
+        }
+    },
+
+    componentWillMount() {
+        this.componentWillReceiveProps(this.props)
+    },
+
+    render() {
+        let {scalingStyle, wrapperStyle, showIFrame} = this.calculateStyle(this.props)
+        let expanded = this.props.canvasItem.expanded
+
+        // If the page is tiny and did not have its iframe loaded already, don't bother loading one. Just show the URL.
+        if (!showIFrame && !this.state.hasIFrame) {
+            return (
+                <div className='webpage-placeholder' title={this.props.url}>
+                    {this.props.url.replace(/^https?:\/\//, '').replace(/^www./, '')}
+                </div>
+            )
+        }
+
+        let iframeSrcPrefix = process.env.MEMEX_PROXY_URL_PREFIX || ''
         // Hard-code exception for included youtube video to reduce proxy load.
         if (this.props.url.startsWith('https://www.youtube.com/embed')) {
             iframeSrcPrefix = ''
@@ -41,7 +71,7 @@ let Webpage = React.createClass({
                         referrerpolicy='no-referrer'
                         sandbox='allow-scripts allow-same-origin'
                     ></iframe>
-                    { !expanded ? <div className='webpage-iframe-overlay'></div> : null}
+                    { !expanded ? <div className='webpage-iframe-overlay' title={this.props.url}></div> : null}
                 </div>
             </div>
         )
